71f3c9bdf8be66cf62518eb3c704c6606810b452
Neurofibromin <125222560+Neurofibromin@users.noreply.github.com>
2024-06-03 17:11:17 +0200

    Changing to crossplatform

---
+++ CursorConverter/Methods.cs
@@ -0,0 +1,103 @@
+﻿using CursorConverter.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ImageMagick;
+
+namespace CursorConverter
+{
+
+
+    public class Methods
+    {
+        public static void Ico2Png(string filepath, string outdirectory)
+        {
+            Console.WriteLine(filepath + outdirectory);
+            Console.WriteLine(Path.Combine(outdirectory, Path.GetFileNameWithoutExtension(filepath) + ".png"));
+            var image = new MagickImage(filepath);
+            image.Format = MagickFormat.Png;
+            image.Write(Path.Combine(outdirectory, Path.GetFileNameWithoutExtension(filepath) + ".png"));
+        }
+
+        public static void xcur2png(string filepath, string outdirectory)
+        {
+            // xcursor theme to png
+            // filepath: location of the xcursor file
+            // only save the highest resolution?
+        }
+        public static void xcur2workingstate(string filepath, string outdirectory)
+        {
+            // extract an xcursor theme into a working state
+        }
+        public static void workingstate2hyprcursor(string filepath, string outdirectory)
+        {
+            // create a compiled hyprcursor theme from a working state
+        }
+
+
+        public enum ChosenFormat
+        {
+            Ani, Ico, XCursor, Cur, Png, Jpg, hyprcursor, CursorFX, CurXPTheme,
+        }
+
+        public static List<string> AllFiles(string FolderToSerach)
+        {
+            string[] files = Directory.GetFiles(FolderToSerach, "*", SearchOption.AllDirectories);
+            return new List<String>(files);
+        }
+
+        public static void ExecutionStarts(List<string> files, string outdirectory, ChosenFormat chosenFormat)
+        {
+            foreach (string file in files)
+            {
+                if (File.Exists(file))
+                {
+                    InvokeAppropriateFunction(file, outdirectory, chosenFormat);
+                }
+            }
+        }
+
+        public static void InvokeAppropriateFunction(string sourcefile, string outdirectory, ChosenFormat chosenFormat)
+        {
+            string extension = Path.GetExtension(sourcefile);
+            Console.WriteLine(extension);
+            extension = extension.Substring(1);
+            switch (extension)
+            {
+                case "ani":
+                    if (chosenFormat == ChosenFormat.Ico)
+                    {
+                        Ani2Ico.Ani2IcoMain(sourcefile, outdirectory);
+                    }
+                    if (chosenFormat == ChosenFormat.XCursor)
+                    {
+                        // aa
+                    }
+                    Console.WriteLine("ani");
+                    break;
+                case "ico":
+                    if (chosenFormat == ChosenFormat.Png)
+                    {
+                        Ico2Png(sourcefile, outdirectory);
+                    }
+                    break;
+
+                case "cur":
+                    break;
+                default:
+                    break;
+            }
+
+            try
+            {
+
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("exception");
+                return;
+            }
+        }
+
+    }
+}
---
+++ CursorConverter/Models/Ani2Ico.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+
+namespace CursorConverter.Models
+{
+
+
+    class Ani2Ico
+    {
+        /* ani2ico: C program created by TeoBigusGeekus - October 2012 *
+        * Extract .ico images from .ani animated Windows cursors.   
+          rewritten to c# by Neurofibromin */
+
+        public static void Ani2IcoMain(string filepath, string outdirectory)
+        {
+            if (!File.Exists(filepath))
+            {
+                Console.Error.WriteLine("Please give an .ani filename as an argument.");
+                return;
+            }
+            
+            
+            ReadFile(filepath, outdirectory);
+            
+            
+        }
+
+        static bool TestForIconString(byte[] buffer, int start)
+        {
+            return buffer[start] == 0x69 && buffer[start + 1] == 0x63 &&
+                   buffer[start + 2] == 0x6f && buffer[start + 3] == 0x6e;
+        }
+
+        static void ReadFile(string name, string outdirectory)
+        {
+            if (!name.EndsWith(".ani", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new FileNotFoundException("Please give an .ani filename as an argument.");
+            }
+
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(name);
+
+            byte[] buffer;
+            try
+            {
+                buffer = File.ReadAllBytes(name);
+            }
+            catch
+            {
+                throw new FileLoadException($"Unable to open file {name}.");
+            }
+
+            int fileLen = buffer.Length;
+            int icoCounter = 1;
+            int maxIcoCounter = 9999;
+            int j = 8;
+
+            for (int i = 0; i <= fileLen; i++)
+            {
+                if (icoCounter == maxIcoCounter)
+                {
+                    return;
+                }
+
+                if (i + 4 <= fileLen && TestForIconString(buffer, i))
+                {
+                    string newIcoName = $"{fileNameWithoutExtension}{icoCounter}.ico";
+                    icoCounter++;
+
+                    try
+                    {
+                        using (FileStream icoImage = new FileStream(Path.Combine(outdirectory, newIcoName), FileMode.Create, FileAccess.Write))
+                        {
+                            j = 8;
+                            while (i + j + 4 <= fileLen)
+                            {
+                                if (TestForIconString(buffer, i + j + 1))
+                                    break;
+                                if (j == 10)
+                                    icoImage.WriteByte(0x01);
+                                else
+                                    icoImage.WriteByte(buffer[i + j]);
+                                j++;
+                            }
+                            if (i + j <= fileLen)
+                                icoImage.WriteByte(buffer[i + j]);
+                            if (fileLen - i - j <= 3)
+                            {
+                                icoImage.WriteByte(buffer[i + j + 1]);
+                                icoImage.WriteByte(buffer[i + j + 2]);
+                            }
+                            icoImage.Flush();
+                        }
+                    }
+                    catch
+                    {
+                        throw new IOException($"Unable to open file {newIcoName}.");
+                    }
+
+                    i += j;
+                }
+            }
+        }
+    }
+
+
+
+}
